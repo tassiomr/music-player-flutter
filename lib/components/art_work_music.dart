d0a412d484d7190895eb1b50c195b7515d03c2bf
tassio.rocha <tassiomarcosr@icloud.com>
2023-08-29 22:37:29 +0200

    created artwork component

---
+++ lib/components/art_work_music.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../models/music.dart';
+
+class ArtWorkMusic extends StatelessWidget {
+  final Music music;
+  const ArtWorkMusic({super.key, required this.music});
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle style = const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+      decoration: TextDecoration.none,
+    );
+
+    return Column(
+        children: [
+          Image.network(music.folderUri, width: 100, height: 100, fit: BoxFit.cover,),
+    const SizedBox(height: 25,),
+    Text(music.trackName, style: style,),
+    Text(music.artistName, style: style,),
+    const SizedBox(height: 25,),
+    ]);
+  }
+}
