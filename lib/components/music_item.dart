e72a3a4ca3d57b463972c3bebf746217ec0dc399
tassio.rocha <tassiomarcosr@icloud.com>
2023-08-29 22:34:35 +0200

    created music item of list

--- lib/components/music_item.dart
+++ lib/components/music_item.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:music_preview_app/models/music.dart";
+import "package:music_preview_app/views/music_player.dart";
 
 class MusicItem extends StatelessWidget {
   const MusicItem({
@@ -19,24 +20,33 @@ class MusicItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center( child: Card(
-      elevation: 0,
-      color: Colors.transparent,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Image.network(music.folderUri),
-            const SizedBox(width: 20,),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("${music.trackName} - ${music.artistName}"),
-                getCollectionName(music.collectionName),
-                Text(music.duration)
-              ],
-            )
-          ],
+    return Center( child: GestureDetector(
+      onTap: () {
+        Navigator.push(context, 
+        MaterialPageRoute(builder: (context) =>  MusicPlayer(music: music)));
+      },
+      child: Card(
+        elevation: 0,
+        color: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(music.folderUri,
+                  )),
+              const SizedBox(width: 20,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("${music.trackName} - ${music.artistName}"),
+                  getCollectionName(music.collectionName),
+                  Text(music.duration)
+                ],
+              )
+            ],
+          ),
         ),
       ),
     ));
