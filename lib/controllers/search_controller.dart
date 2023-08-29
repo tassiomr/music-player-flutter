15e05c353b3faf0dff47f1f69a64c276b72672e5
tassio.rocha <tassiomarcosr@icloud.com>
2023-08-29 15:45:51 +0200

    finish search controller

--- lib/controllers/search_controller.dart
+++ lib/controllers/search_controller.dart
@@ -1,13 +1,18 @@
-import 'package:http/http.dart' as http;
-import 'package:music_preview_app/models/music.dart';
-import 'package:music_preview_app/repositories/search_musics.dart';
-import 'package:music_preview_app/services/network.dart';
+import 'dart:convert';
 
-class SearchController {
+import 'package:music_preview_app/models/music.dart';
+import 'package:music_preview_app/models/result.dart';
+import 'package:music_preview_app/repositories/search_musics.dart';
+
+class SearchMusicController {
+
    final SearchMusicImpl _repository = SearchMusicImpl();
 
-  List getMusic (String term) {
-    var result = _repository.getMusics(term);
-    return [];
+  Future<List<Music>> getMusic (String term) async {
+    var result = await _repository.getMusics(term);
+    var decodeResult = Result.fromJson(jsonDecode(result.body));
+
+    print(decodeResult.resultCount);
+    return decodeResult.results;
   }
 }
