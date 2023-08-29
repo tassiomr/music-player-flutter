1a27709f349e5d65e2dea6ecb66922302d9c4490
tassio.rocha <tassiomarcosr@icloud.com>
2023-08-29 18:49:00 +0200

    add duration on music file

--- lib/models/music.dart
+++ lib/models/music.dart
@@ -5,7 +5,7 @@ class Music {
   final String folderUri;
   final String previewSong;
   final String collectionName;
-
+  final String duration;
   const Music({
     required this.artistName,
     required this.trackId,
@@ -13,17 +13,25 @@ class Music {
     required this.folderUri,
     required this.previewSong,
     required this.collectionName,
+    required this.duration,
   });
 
   factory Music.fromJson(Map<String, dynamic> json) {
     return Music(
-      artistName: json['artistName'],
-      trackId: json['trackId'],
-      trackName: json['trackName'],
-      folderUri: json['artworkUrl100'],
-      previewSong: json['previewUrl'],
-      collectionName: json['collectionName'],
+      artistName: json['artistName']??"",
+      trackId: json['trackId']??"",
+      trackName: json['trackName']??"",
+      folderUri: json['artworkUrl100']??"",
+      previewSong: json['previewUrl']??"",
+      collectionName: json['collectionName']??"",
+      duration: Music.parseMillisToTime(json['trackTimeMillis']??0)
     );
+  }
+
+  static String parseMillisToTime (int millis) {
+
+
+    return "3:32";
   }
 
 }
