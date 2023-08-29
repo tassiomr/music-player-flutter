179da77528f94f24e5c0194b728a53b9ade0f92d
tassio.rocha <tassiomarcosr@icloud.com>
2023-08-29 22:33:34 +0200

    update loading componet with gradient bkg

--- lib/components/loading.dart
+++ lib/components/loading.dart
@@ -5,8 +5,26 @@ class Loading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(),
+    return Container(
+      decoration: BoxDecoration(
+    gradient: LinearGradient(
+    begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        stops: const [
+          0.1,
+          0.5,
+          0.7,
+          0.9
+        ],
+        colors: [
+          Colors.purple.shade100,
+          Colors.red.shade100,
+          Colors.green.shade100,
+          Colors.orange.shade100.withOpacity(0.9),
+        ])),
+      child: const Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
