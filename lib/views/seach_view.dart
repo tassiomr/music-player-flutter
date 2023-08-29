1986e7ff69f183324dcc15a3ad97ccd9e65fc59b
tassio.rocha <tassiomarcosr@icloud.com>
2023-08-29 18:51:24 +0200

    created components to search a music

--- lib/views/seach_view.dart
+++ lib/views/seach_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:music_preview_app/components/loading.dart';
+import 'package:music_preview_app/components/music_item.dart';
 import 'package:music_preview_app/controllers/search_controller.dart';
 
 import '../models/music.dart';
@@ -12,7 +13,6 @@ class SearchView extends StatefulWidget {
 }
 
 class _SearchViewState extends State<SearchView> {
-
   List<Music> musics = [];
   bool isLoading = false;
 
@@ -22,15 +22,14 @@ class _SearchViewState extends State<SearchView> {
       setState(() {
         isLoading = true;
       });
-      var result = await controller.getMusic("never really over");
-      //
+
+      var result = await controller.getMusic("katy perry");
+
       setState(() {
         isLoading = false;
         musics = result;
       });
-
-
-    } catch(_, e) {
+    } catch (eita, e) {
       setState(() {
         isLoading = false;
       });
@@ -38,29 +37,80 @@ class _SearchViewState extends State<SearchView> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getMusic();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     Widget child;
 
-    if(musics.isEmpty) {
+    if (musics.isEmpty) {
       child = const Text('Nothing to see yet');
     } else {
-      child = const Placeholder();
+      child = ListView(
+        children: [for (var music in musics) MusicItem(music: music)],
+      );
     }
 
-    if(isLoading) {
-      return  const Loading();
+    if (isLoading) {
+      return const Loading();
     }
 
-    return Center(
-      child: Column(
-        children: [
-          ElevatedButton(onPressed: getMusic, child: const Text('procurar')),
-          child
-        ],
+    return Container(
+      color: Theme.of(context).colorScheme.inversePrimary,
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: ListView(
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              const Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: "Search a music or artist",
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 20,
+                              ),
+                              SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: Material(
+                                    color: Theme.of(context).colorScheme.inversePrimary,
+                                    child: Ink(
+                                      decoration: const ShapeDecoration(
+                                        color: Colors.brown,
+                                        shape: CircleBorder(),
+                                      ),
+                                      child: IconButton(
+                                        icon: const Icon(Icons.search),
+                                        onPressed: getMusic,
+                                        color: Colors.grey[200],
+                                      ),
+                                    ),
+                                  )),
+                            ],
+                          ))),
+                  for (var music in musics) MusicItem(music: music)
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
-
-
   }
 }
