3e372e5a0fa8b542c671d177ffe10579159a2058
tassio.rocha <tassiomarcosr@icloud.com>
2023-08-29 18:50:28 +0200

    created item music render

--- lib/components/music_item.dart
+++ lib/components/music_item.dart
@@ -1,8 +1,44 @@
+import "package:flutter/material.dart";
+import "package:music_preview_app/models/music.dart";
+
 class MusicItem extends StatelessWidget {
-  const MusicItem({super.key});
+  const MusicItem({
+    super.key,
+    required this.music
+  });
+
+  final Music music;
+
+  Widget getCollectionName(String name) {
+    if(name.isNotEmpty) {
+      return Text(name);
+    } else {
+      return const SizedBox(width: 0, height: 0,);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center( child: Card(
+      elevation: 0,
+      color: Colors.transparent,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Image.network(music.folderUri),
+            const SizedBox(width: 20,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("${music.trackName} - ${music.artistName}"),
+                getCollectionName(music.collectionName),
+                Text(music.duration)
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
