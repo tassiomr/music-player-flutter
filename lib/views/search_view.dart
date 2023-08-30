68130eccf456825e837db14c1ca8bdec7af494c8
tassio.rocha <tassiomarcosr@icloud.com>
2023-08-30 22:13:33 +0200

    update search view to use bloc and cubit

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -4,21 +4,8 @@ import 'package:music_preview_app/components/bottom_music_sheet.dart';
 import 'package:music_preview_app/components/loading.dart';
 import 'package:music_preview_app/components/music_item.dart';
 import 'package:music_preview_app/cubits/search_musics_cubit.dart';
-import 'package:music_preview_app/repositories/search_musics.dart';
 
 import '../components/searching.dart';
-
-// class SearchViewProvider extends StatelessWidget {
-//   const SearchViewProvider({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocProvider(
-//       create: (_) => SearchMusicsCubit(context.read<SearchMusicImpl>()),
-//       child: const SearchView(),
-//     );
-//   }
-// }
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -28,6 +15,11 @@ class SearchView extends StatelessWidget {
     Widget child;
     return BlocBuilder<SearchMusicsCubit, SearchMusicState>(
         builder: (context, state) {
+          context.read<SearchMusicsCubit>().requestMusics("Joe Jonas");
+
+          if(state.musics.isNotEmpty && state.music == null) {
+            context.read<SearchMusicsCubit>().selectMusic(state.musics[0]);
+          }
       if (state.isLoading!) {
         return const Loading();
       }
@@ -50,7 +42,7 @@ class SearchView extends StatelessWidget {
               Colors.orange.shade100.withOpacity(0.9),
             ])),
         child: Padding(
-          padding: const EdgeInsets.all(24.0),
+          padding: const EdgeInsets.all(0),
           child: ListView(
             children: [
               Column(
